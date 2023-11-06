d79e647688459b4ce91515ec6349b0ce107f495d
m.ghasemi <mehdi.scoob@gmail.com>
2023-11-06 18:00:03 +0330

    bug fix

---
+++ app/Http/Controllers/Trip/TripController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Trip;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Trip\CreateTripRequest;
+use Illuminate\Http\Request;
+
+class TripController extends Controller
+{
+
+
+    public function createTrip(CreateTripRequest $request)
+    {
+
+    }
+}
